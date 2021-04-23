5354f20ec7ea73a27c3757e1ce9d3c8d46244d3b
vtumanov782 <vtumanov782@gmail.com>
2021-04-23 17:18:09 +0300

    make a dot move

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import './App.css';
+import {Scene} from '../Scene';
+import { Transpose } from '../../operator/Transpose';
+import { SpawnEntity } from '../../operator';
 
 export function App() {
+  const operator = new Transpose({
+    propName: 'position',
+    vector: {
+      x: 10,
+      y: 20
+    },
+    framesCount: 10
+  }, [
+    new SpawnEntity({
+      types: ['dot'],
+      payload: {
+        position: {
+          x: 20,
+          y: 30
+        }
+      }
+    })
+  ])
   return (
     <div className="App">
       <span>React application</span>
+      <Scene operator={operator} />
     </div>
   );
 }
---
+++ src/components/Scene/Scene.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { Frame } from '../../frame';
+import { Operator } from '../../operator/Base';
+
+interface SceneProps {
+    operator: Operator<any>
+}
+
+interface SceneState {
+    frames: Array<Frame>
+    currentFrame: number
+}
+
+export class Scene extends React.Component<SceneProps, SceneState> {
+    canvasRef: React.RefObject<HTMLCanvasElement>;
+
+    constructor(props: SceneProps) {
+        super(props);
+        const {frame} = props.operator.tick({});
+        const frames = frame ? [frame] : [];
+
+        this.state = {
+            frames,
+            currentFrame: 0
+        };
+
+        this.canvasRef = React.createRef<HTMLCanvasElement>();
+    }
+
+    componentDidMount() {
+        this.repaintCanvas();
+    }
+
+    onClickNext = () => {
+        if (this.state.currentFrame < this.state.frames.length - 1) {
+            this.setState((currentState) => ({
+                currentFrame: currentState.currentFrame + 1
+            }));
+        }
+
+        const {done, frame} = this.props.operator.tick({});
+        if (done) {
+            return;
+        }
+        if (frame) {
+            this.setState(currentState => ({
+                frames: [...currentState.frames, frame],
+                currentFrame: currentState.currentFrame + 1
+            }));
+        }
+    }
+
+    onClickPrevious = () => {
+        this.setState(currentState => ({
+            currentFrame: Math.max(0, currentState.currentFrame - 1)
+        }));
+    }
+
+    componentDidUpdate() {
+        this.repaintCanvas();
+    }
+
+    getCurrentFrame(): Frame {
+        return this.state.frames[this.state.currentFrame];
+    }
+
+    clearCanvas() {
+        const canvas = this.canvasRef.current;
+        if (!canvas) return;
+        const ctx = canvas.getContext('2d');
+        if (!ctx) return;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    repaintCanvas() {
+        const canvas = this.canvasRef.current;
+        if (!canvas) return;
+        const ctx = canvas.getContext('2d');
+        if (!ctx) return;
+        this.clearCanvas();
+
+        const frame = this.getCurrentFrame();
+        frame.entities.forEach(entity => {
+            if (entity.types.includes('dot')) {
+                ctx.fillRect(
+                    entity.payload.position.x,
+                    entity.payload.position.y,
+                    1,
+                    1
+                )
+            }
+        });
+    }
+
+    render() {
+        const frame = this.getCurrentFrame();
+        return (
+            <div>
+                <button onClick={this.onClickPrevious}>PREV</button>
+                <button onClick={this.onClickNext}>NEXT</button>
+                <canvas ref={this.canvasRef} width="150" height="150"></canvas>
+                <pre>
+                    {JSON.stringify(frame, null, 4)}
+                </pre>
+            </div>
+        );
+    }
+}
